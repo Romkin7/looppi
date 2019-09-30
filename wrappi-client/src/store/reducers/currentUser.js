81e925b488cb24a9ac151cec431b5b7bd0bccf80
Roman Tuomisto <romantuomisto@Romans-MacBook-Air.local>
2019-09-30 18:04:55 +0300

    ready to deploy

--- wrappi-client/src/store/reducers/currentUser.js
+++ wrappi-client/src/store/reducers/currentUser.js
@@ -7,12 +7,13 @@ const DEFAULT_STATE = {
 
 export default (state = DEFAULT_STATE, action) => {
     switch (action.type) {
-        case SET_CURRENT_USER:
-            return {
-                isAuthenticated: !!Object.keys(action.user).length,
-                user: action.user
-            };
-        default:
-            return state;
+      case SET_CURRENT_USER:
+        return {
+          // turn empty object into false or if there are keys, true
+          isAuthenticated: !!Object.keys(action.user).length,
+          user: action.user
+        };
+      default:
+        return state;
     }
-};
+  };
