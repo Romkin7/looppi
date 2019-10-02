9b29a13b5ddde0fc2b4d3f56d1e32a1e2da1bcbf
Roman Tuomisto <romantuomisto@Romans-MacBook-Air.local>
2019-10-02 22:11:05 +0300

    timer done 3

--- wrappi-client/src/containers/Main.js
+++ wrappi-client/src/containers/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-
+import { logout } from "../store/actions/auth";
 import { authenticateUser } from "../store/actions/auth";
 import { removeError } from "../store/actions/errors";
 
@@ -11,14 +11,31 @@ import AppNavbar from "./AppNavbar";
 import Footer from "./Footer";
 
 class Main extends Component {
-    state = {
-        maxResult: "",
-        min: 0,
-        max: 0,
-        multiplier: 0,
-        amountOfNumbers: 2,
-        operator: "",
-        bgColor: "blue"
+    constructor(props) {
+        super(props);
+        this.state = {
+            maxResult: "",
+            min: 0,
+            max: 0,
+            multiplier: 0,
+            amountOfNumbers: 2,
+            operator: "",
+            bgColor: "blue"
+        }
+    }
+
+    logoutFromGame = () => {
+        this.props.logout();
+        this.setState({
+            maxResult: "",
+            min: 0,
+            max: 0,
+            multiplier: 0,
+            amountOfNumbers: 2,
+            operator: "",
+            bgColor: "blue"
+        });
+        this.props.history.push("/");
     }
     
     setParameters = (newMaxResult, newOperator, newBgColor, newAmountOfNumbers, newMin, newMax, newMultiplier) => {
@@ -39,14 +56,14 @@ class Main extends Component {
         const title = operator === "addition" 
         ? "Yhteenlaskut 0–" 
         : operator === "substraction" 
-        ? "Vähennyslaskut 0–10" 
+        ? "Vähennyslaskut 0–" 
         : operator === "multiplication"
         ? "Kertotaulut "
         : operator === "division" ? "Jakolaskut 0–10"
         : "Looppi - Laskuja lapsille";
         return (
             <div className="mainContainer">
-                <AppNavbar titleText={title} maxResult={ maxResult === 0 ? "" : maxResult } bgColor={ bgColor } currentUser={currentUser}></AppNavbar>
+                <AppNavbar titleText={title} logout={this.logoutFromGame} maxResult={ multiplier ? multiplier : max } bgColor={ bgColor } currentUser={currentUser}></AppNavbar>
                 <Switch>
                     <Route path="/" exact render={props => {
                     return (<Login removeError={ removeError } errors={ errors } 
@@ -74,4 +91,4 @@ function mapStateToProps(state) {
     };
 };
 
-export default withRouter(connect(mapStateToProps, { authenticateUser, removeError })(Main));
+export default withRouter(connect(mapStateToProps, { authenticateUser, removeError, logout })(Main));
