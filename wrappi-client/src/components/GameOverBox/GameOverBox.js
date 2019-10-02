38a064374c5d867cd17d86becbde31324d4fe891
Pauliina Veijalainen <pauliina.veijalainen@gmail.com>
2019-10-02 15:35:48 +0300

    gameoverboxin grafiikoita

--- wrappi-client/src/components/GameOverBox/GameOverBox.js
+++ wrappi-client/src/components/GameOverBox/GameOverBox.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './GameOverBox.css';
 
 const GameOverBox = (props) => {
     return (
-        <div id="background">
+        <div id="gameoverContainer">
             <div className="gameoverbox">
-                <h5>Hienosti laskettu!</h5>
+                <h4>Hienosti laskettu!</h4>
                 <p>Teit XX laskua XX ajassa. Nopeutesi oli XX laskua minuutissa.</p>
                 <p>Lähetä tuloksesi itsellesi tai opettajallesi:</p>
                 <input type="text" value="email" placeholder="Kirjoita sähköpostiosoite"></input>            
-                <button>Takaisin</button>
-                <button>Lähetä</button>
+                <button className="submit">Lähetä</button>
+                <button className="endButton">Takaisin</button>
+
             </div>
         </div>
     )
