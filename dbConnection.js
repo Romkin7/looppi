b36494f8780e78d040af288ca8075e0da8c47305
Roman Tuomisto <romantuomisto@Romans-MacBook-Air.local>
2019-10-03 22:10:48 +0300

    looppi ready to deploy

--- dbConnection.js
+++ dbConnection.js
@@ -1,24 +1,24 @@
 //Dateabase connection
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-//    socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock',
-    host: "127.0.0.1",
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: "wrappi_db"
-});
+// const connection = mysql.createConnection({
+// //    socketPath : '/Applications/MAMP/tmp/mysql/mysql.sock',
+//     host: "127.0.0.1",
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: "wrappi_db"
+// });
 
-connection.connect();
+// connection.connect();
 
-// Add or get user
-module.exports.setUser = async (req, res, next) => {
-    try {
-        let user = await connection.query("SELECT Username FROM users WHERE "+req.body.username);
-    } catch(err) {
-        return next(err);
-    }
-};
+// // Add or get user
+// module.exports.setUser = async (req, res, next) => {
+//     try {
+//         let user = await connection.query("SELECT Username FROM users WHERE "+req.body.username);
+//     } catch(err) {
+//         return next(err);
+//     }
+// };
 
-connection.end();
+// connection.end();
