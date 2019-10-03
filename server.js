b92a01717f48722ef09c48824d22064163f7b17c
Roman Tuomisto <romantuomisto@Romans-MacBook-Air.local>
2019-10-03 22:34:52 +0300

    looppi ready to deploy

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use("/api", authRoutes);
 
 //Serve react app in production to the browser
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname, "looppi-client/build"));
+    app.use(express.static(__dirname, "/looppi-client/build"));
     app.get("*", (req, res) => {
         res.sendFile("index.html");
     });
